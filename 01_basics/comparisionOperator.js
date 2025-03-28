a1567ed6b1c8211f259d4e4bdaf14e068d233a87
Brajendra Sharma <brajendrasharmait76@gmail.com>
2025-03-28 10:13:39 +0000

    comparisionOperator

---
+++ 01_basics/comparisionOperator.js
@@ -0,0 +1,14 @@
+// <, >, <=, >=, ==, !=
+// console.log("2" > 1) // true
+// console.log("02" > 1) // true
+// console.log(null > 0); // false
+// console.log(null < 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true here javascript work differently they treated null as a 0, but >,== they treated differently.
+
+// console.log(undefined == 0) //false
+// console.log(undefined < 0) //false
+// console.log(undefined > 0) //false
+
+// ===
+console.log("2" === 2); //false 
