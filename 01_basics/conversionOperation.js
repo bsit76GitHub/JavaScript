6cebf2ae372fe5423713db8c5562074d99630fd1
Brajendra Sharma <brajendrasharmait76@gmail.com>
2025-03-28 09:54:07 +0000

    assignementOperator

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,18 +1,18 @@
-let score = ""
-console.log(typeof score); //string
-//console.log(typeof(score)); //string
+// let score = ""
+// console.log(typeof score); //string
+// //console.log(typeof(score)); //string
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber); 
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber);
 
-let valueInBoolean = Boolean(score)
-console.log(typeof valueInBoolean); 
-console.log(valueInBoolean);
+// let valueInBoolean = Boolean(score)
+// console.log(typeof valueInBoolean); 
+// console.log(valueInBoolean);
 
-let valueInString = String(score)
-console.log(typeof valueInString); 
-console.log(valueInString);
+// let valueInString = String(score)
+// console.log(typeof valueInString); 
+// console.log(valueInString);
 
 
 
@@ -39,3 +39,37 @@ true/false   =>   boolean            =>                boolean     =>        tru
 ""           =>   string             =>                boolean     =>        false
                              
 */
+//***************************   Operations    ****************** */
+
+let value = 3
+let negValue = - value
+//console.log(negValue); //-3
+
+let str1 = "Hello"
+let str2 = "JavaScript"
+let str3 = str1 + str2
+// console.log(str3); //HelloJavaScript
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+
+// console.log("1" + 2 + 3); //123
+// console.log(1+ 2 + "3"); //33
+
+// console.log(true); //true
+// console.log(false); //false
+// console.log(+true); // 1
+// console.log(-true); // -1
+// console.log(+false); // 0
+// console.log(-false); // -0
+// console.log(+""); // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 //valid but not prefered to write
+
+let x = 2
+let y = x++
+console.log(x, y); // 3 2
+let a = 2
+let b = ++a
+console.log(a, b);  // 3 3
