b3e9c20d6b8f3213b3308fd4acbf6d6e40fd38fc
Brajendra Sharma <brajendrasharmait76@gmail.com>
2025-04-16 09:23:59 +0000

    addingProjeect4Solution

--- praticsProject/script.js
+++ praticsProject/script.js
@@ -6,31 +6,31 @@ project.forEach(function (a) {
             colorSwitcher.style.display = "block"
             bmicalculator.style.display = 'none'
             digitalClock.style.display = 'none'
-            guessNumber.style.display = 'none'
+            gussingNumber.style.display = 'none'
         }
         if (e.target.id === 'project2') {
             colorSwitcher.style.display = 'none'
             bmicalculator.style.display = "block"
             digitalClock.style.display = 'none'
-            guessNumber.style.display = 'none'
+            gussingNumber.style.display = 'none'
         }
         if (e.target.id === 'project3') {
             colorSwitcher.style.display = "none"
             bmicalculator.style.display = 'none'
             digitalClock.style.display = "block"
-            guessNumber.style.display = 'none'
+            gussingNumber.style.display = 'none'
         }
         if (e.target.id === 'project4') {
             colorSwitcher.style.display = "none"
             bmicalculator.style.display = 'none'
             digitalClock.style.display = 'none'
-            guessNumber.style.display = "block"
+            gussingNumber.style.display = "block"
 
         }
     })
 })
 
-
+//==================================================
 const body = document.querySelector('body')
 const buttons = document.querySelectorAll('.button')
 buttons.forEach(function (button) {
@@ -51,6 +51,7 @@ buttons.forEach(function (button) {
         }
     })
 })
+//==================================================
 const form = document.querySelector('form');
 // const height = parseInt(document.querySelector("#height").value)  // this usecase will give you empty
 form.addEventListener("submit", function (e) {
@@ -77,13 +78,95 @@ form.addEventListener("submit", function (e) {
     }
 
 })
-
+//==================================================
 const clock = document.getElementById('clock')
 setInterval(function(){
     let date = new Date();
     clock.innerHTML = date.toLocaleTimeString();
 },1000)
 
+//==================================================
+let randomNumber = parseInt(Math.random() * 100 + 1);
 
+const submit = document.querySelector('#submit');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
 
+const p = document.createElement('p');
+
+let prevGuess = [];
+let numGuess = 1;
+let playGame = true;
+
+if (playGame) {
+    submit.addEventListener('click',function (e) {
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        validateGuess(guess);
+    })
+}
+function validateGuess(guess) {
+    if (isNaN(guess)) {
+        alert('Please enter a valid number')
+    } else if (guess < 1) {
+        alert('Please enter a number greater than 1')
+    }else if (guess > 100) {
+        alert('Please enter a number less than 100')
+    } else {
+        prevGuess.push(guess);
+        if (numGuess === 11) {
+            displayGuess(guess);
+            displayMessage(`Game Over. Random number was ${randomNumber}`);
+            endGame();
+        } else{
+            displayGuess(guess);
+            checkGuess(guess);
+        }
+    }
+}
+function checkGuess(guess) {
+    if (guess === randomNumber) {
+        displayMessage(`you guessed it right`);
+        endGame();
+    } else if(guess < randomNumber) {
+        displayMessage(`Number is too low`);
+    } else if(guess > randomNumber){
+        displayMessage(`Number is too high`);
+    }
+}
+function displayGuess(guess) {
+    userInput.value = '';
+    guessSlot.innerHTML += `${guess}, `;
+    numGuess++;
+    remaining.innerHTML = `${11 - numGuess}`;
+}
+function displayMessage(message) {
+    lowOrHi.innerHTML = `<h2>${message}</h2>`; 
+}
+function endGame() {
+  userInput.value = '';
+  userInput.setAttribute('disabled', '');
+  p.classList.add('button');
+  p.innerHTML = `<h2 id="newGame">Start new Game</h2>`;
+  startOver.appendChild(p);
+  playGame = false;
+  newGame();
+}
+
+function newGame() {
+    const newGameButton = document.querySelector('#newGame');
+    newGameButton.addEventListener('click',function (e) {
+        randomNumber = parseInt(Math.random() * 100 + 1);
+        prevGuess = [];
+        numGuess = 1;
+        guessSlot.innerHTML = '';
+        remaining.innerHTML = `${11 - numGuess} `;
+        userInput.removeAttribute('disabled');
+        startOver.replaceChild(p);
+        playGame = true       
+    });
+}
 
